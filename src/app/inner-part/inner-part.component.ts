e6cba1919dbd43777244f7b7caab3985bc6375c3
zhivar <zhivarsourati@gmail.com>
2020-01-29 20:31:04 +0330

    download the picture added

--- src/app/inner-part/inner-part.component.ts
+++ src/app/inner-part/inner-part.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { NotifierService } from "angular-notifier";
+import { ReadVarExpr } from "@angular/compiler";
 
 @Component({
   selector: "app-inner-part",
@@ -11,13 +12,25 @@ export class InnerPartComponent implements OnInit {
 
   ngOnInit() {}
 
+  mouse_on_image = false;
   files: any = [];
   image_url: String | ArrayBuffer = "";
+  correct_image_types = ["image/jepg", "image/png"];
+
+  is_file_type_ok(event) {
+    if (this.correct_image_types.includes(event.type)) return true;
+    this.notifier.notify(
+      "error",
+      "file type is not supported, you can only upload images with "
+    );
+    return false;
+  }
 
   uploadFile(event) {
     if (event) {
+      if (!this.is_file_type_ok(event)) return;
       console.log("TCL: InnerPartComponent -> uploadFile -> event", event);
-      this.files.push(event.name);
+      this.files.push(event);
       var reader = new FileReader();
 
       reader.readAsDataURL(event); // read file as data url
@@ -30,9 +43,25 @@ export class InnerPartComponent implements OnInit {
       this.notifier.notify("success", "Image uploaded successfully");
     }
   }
+
+  downloadFile() {
+    console.log(
+      "TCL: InnerPartComponent -> downloadFile -> this.image_url",
+      this.image_url
+    );
+    window.open(<string>this.image_url, "_blank");
+  }
+
   deleteAttachment(index) {
     this.files.splice(index, 1);
     this.image_url = "";
     this.notifier.notify("success", "Image removed successfully");
+  }
+
+  mouse_enter() {
+    this.mouse_on_image = true;
+  }
+  mouse_leave() {
+    this.mouse_on_image = false;
   }
 }
